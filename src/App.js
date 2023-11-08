c45f5c4c5eb0b7c3515189890c5478332f45bccc
Mina Magdy <minamagdy57264@gmail.com>
2023-11-08 18:21:09 +0200

    monmon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from 'react'
+import NavBar from "./component/NavBar";
+import MoviesList from "./component/MoviesList";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [movies,setMovies]=useState([])
+
+  //get all movies by axios
+  const getAllMovies= async ()=>{
+   const res=await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=63f0deff8982f597ed984fc7a7c983f1&include_adult=false&include_video=false&language=ar&sort_by=popularity.desc')
+    setMovies(res.data.results)
+  }
+  useEffect(()=>{
+    getAllMovies();
+  },[])
+
+  const Search=async(word)=>{
+    if(word===""){
+      getAllMovies();
+    }else{ const res=await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=63f0deff8982f597ed984fc7a7c983f1%20&query=${word}&language=ar`)
+    setMovies(res.data.results)}
+  }
+
+  return(
+    <div className="font color-body ">
+      <NavBar Search={Search}/>
+      <Container>
+              <MoviesList movies={movies} />
+      </Container>
     </div>
   );
-}
+  }
 
 export default App;
